d2f6395a85b3fd641b2f17a01303975941fcf21c
Disha <dishatalreja1202@gmail.com>
2021-11-01 15:22:58 +0530

    Implemented the UI for reroute fulfilment app(#1ttma4h)

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,39 +1,136 @@
 <template>
   <ion-page>
-    <ion-header :translucent="true">
-      <ion-toolbar>
-        <ion-title>{{ $t("Blank") }}</ion-title>
-      </ion-toolbar>
-    </ion-header>
+    
     
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">{{ $t('Blank') }}</ion-title>
-        </ion-toolbar>
-      </ion-header>
+      <ion-text id="your-order">
+        <h1> Your Order</h1>
+      </ion-text>
+  
+    <ion-card>
     
-      <div id="container">
-        <strong>{{ $t('Ready to create an app?') }}</strong>
-        <p>{{ $t('Start with Ionic') }} <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">{{ $t('UI Components') }}</a></p>
-      </div>
+
+       <ion-item lines="none">
+         <ion-label slot="start" >
+          
+             <h2>customer name</h2>
+           
+           
+             
+            <p>order ID</p> 
+
+          
+         </ion-label>
+         <ion-label slot="end" color="medium" position="fixed">
+            order date
+         </ion-label>
+         
+            <!-- <p>order date</p>  -->
+        
+         
+       </ion-item>
+
+       <ion-item lines="full">
+         <ion-label>
+           <ion-thumbnail>
+             <img src="https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y" alt="">
+           </ion-thumbnail>
+         </ion-label>
+         <ion-label>
+           <ion-label slot="start">
+             <ion-label position="stacked">BRAND</ion-label>
+             <list-item-title>
+               <h2>Virtual Name</h2>
+               
+               </list-item-title>
+             <ion-label color="medium">color:color</ion-label>
+             <ion-label color="medium">Size:size</ion-label>
+
+           </ion-label>
+           
+         </ion-label>
+         
+             <ion-badge slot="end" color="primary">status</ion-badge>
+          
+       </ion-item>
+
+       <ion-item>
+         
+           <ion-label slot="start">
+             <!-- <list-item-title> -->
+               Store pickup
+             <!-- </list-item-title> -->
+           </ion-label>
+
+           <ion-button @click="openPopover" slot="end" color="medium" fill="outline">
+             change
+             
+           </ion-button>
+           
+
+          
+           
+
+        
+
+       </ion-item>
+
+
+       <ion-item lines="full">
+        <ion-label slot="start">
+          Ship to
+        </ion-label>
+        
+          <ion-button slot="end" color="medium" fill="outline">EDIT</ion-button>
+
+       
+       </ion-item>
+       <ion-item lines="none">
+         <ion-label color="danger">
+         CANCEL ITEM
+             
+          
+         </ion-label>
+       </ion-item>
+
+
+  
+    </ion-card>
+    
+     
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import { IonContent,IonPage,  popoverController } from '@ionic/vue';
 import { defineComponent } from 'vue';
+import Popover from './popover.vue'
+
 
 export default defineComponent({
-  name: 'Home',
+
   components: {
     IonContent,
-    IonHeader,
     IonPage,
-    IonTitle,
-    IonToolbar
-  }
+   
+  },
+  methods: {
+    async openPopover(ev: Event) {
+      const popover = await popoverController
+        .create({
+          component: Popover,
+          cssClass: 'my-custom-class',
+          event: ev,
+          translucent: true
+        })
+      await popover.present();
+
+      const { role } = await popover.onDidDismiss();
+      console.log('onDidDismiss resolved with role', role);
+    },
+  },
+  
 });
 </script>
 
@@ -65,4 +162,31 @@ export default defineComponent({
 #container a {
   text-decoration: none;
 }
+#your-order{
+  position: absolute;
+width: 123px;
+height: 31px;
+left: calc(50% - 123px/2 + 0.5px);
+top: 128px;
+visibility: visible;
+}
+#your-order h1{
+  font-size: 26px;
+line-height: 120%;
+/* or 31px */
+
+text-align: center;
+color: rgba(0, 0, 0, 0.87);
+}
+ion-content{
+visibility: visible;
+}
+#label{
+  width: 100px;
+  padding: 0;
+}
+.myCustomSelect{
+  max-width: 100% !important;
+}
+
 </style>
---
+++ src/views/popover.vue
@@ -0,0 +1,34 @@
+<template>
+  <ion-content class="ion-padding">
+    <ion-list>
+        <ion-item>
+            <ion-icon slot="start" :icon="storefrontOutline"></ion-icon>
+            <ion-label>Change Store</ion-label>
+        </ion-item>
+        <ion-item>
+            <ion-icon slot="start" :icon="mailOutline"></ion-icon>
+            <ion-label>Get it shipped</ion-label>
+        </ion-item>
+    </ion-list>
+  </ion-content>
+</template>
+
+<script>
+import { IonContent } from '@ionic/vue';
+import { defineComponent } from 'vue';
+import { 
+ storefrontOutline,
+ mailOutline,
+} from 'ionicons/icons';
+
+export default defineComponent({
+  name: 'Popover',
+  components: { IonContent },
+  setup() {
+    return {
+      storefrontOutline,
+      mailOutline
+    }
+  }
+});
+</script>
