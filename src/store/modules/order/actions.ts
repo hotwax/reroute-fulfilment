399c869bed9047a3da2bfe38255b9e24629865e6
Yash Maheshwari <yash.maheshwari@hotwax.co>
2021-12-24 19:02:16 +0530

    Updated: code to define iteration before ion-card and making the cancel item available to each item card(#1wfamfm)

--- src/store/modules/order/actions.ts
+++ src/store/modules/order/actions.ts
@@ -21,7 +21,7 @@ const actions: ActionTree<OrderState , RootState> ={
       resp = await OrderService.getOrder(obj);
       const order = resp.data;
       if (resp.status === 200 && resp.data && !hasError(resp)) {
-        commit(types.OPEN_ORDERS_INITIAL, { current: resp.data })
+        commit(types.ORDER_CURRENT, { current: resp.data })
         let productIds: any = new Set();
         order.shipGroup.map((group: any) => {
           group.items.map((item: any) => {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,44 +2,44 @@
   <ion-page>
     <ion-content :fullscreen="true">
       <h1>{{ $t("Your Order") }}</h1>
-      <ion-card>
-        <ion-item lines="none">
-          <ion-label>
-            <h1>{{ order.customerName }}</h1>
-            <p>{{ order.id }}</p>
-          </ion-label>
-            <ion-note slot="end">{{ order.orderDate }}</ion-note>
-        </ion-item>
-        <div v-for="(shipGroup, index) of order.shipGroup" :key="index">
-        <ion-item v-for="item of shipGroup.items" :key="item.id" lines="full">
-          <ion-thumbnail slot="start">
-            <Image :src="getProduct(item.productId).mainImageUrl" />
-          </ion-thumbnail>
-          <ion-label slot="start">
-            <p>{{item.brandName}}</p>
-            <h2>{{ item.name }}</h2>
-            <p>{{ $t("Color") }} : {{ $filters.getFeatures(getProduct(item.productId).featureHierarchy, '1/COLOR/') }}</p>
-            <p>{{ $t("Size") }} : {{ $filters.getFeatures(getProduct(item.productId).featureHierarchy, '1/SIZE/') }}</p>
-          </ion-label>
-          <ion-badge slot="end">{{item.status}}</ion-badge>
-        </ion-item>
-        <ion-item>
-          <ion-label>{{ shipGroup.shipmentMethodTypeId }} </ion-label>
-          <ion-button @click="changePickupPreference"  color="medium" fill="outline">{{ $t("Change") }}</ion-button>
-        </ion-item>
-        <ion-item>
-          <ion-list>
-          <ion-label>{{ shipGroup.shipTo.postalAddress.toName }}</ion-label>
-          <ion-label>{{ shipGroup.shipTo.postalAddress.address1 }} </ion-label>
-          <ion-label>{{ shipGroup.shipTo.postalAddress.city}} {{ shipGroup.shipTo.postalAddress.country}} {{ shipGroup.shipTo.postalAddress.postalCode}}</ion-label>
-          </ion-list>
-          <ion-button  slot="end" color="medium" fill="outline" @click="updateShipmentAddress">{{ $t("Edit") }}</ion-button>
-        </ion-item>
-        </div>
-        <ion-item lines="none">
-          <ion-label color="danger">{{ $t("Cancel Item") }}</ion-label>
-        </ion-item>
-      </ion-card>
+      <ion-item lines="none">
+        <ion-label>
+          <h1>{{ order.customerName }}</h1>
+          <p>{{ order.id }}</p>
+        </ion-label>
+          <ion-note slot="end">{{ order.orderDate }}</ion-note>
+      </ion-item>
+      <div v-for="(shipGroup, index) of order.shipGroup" :key="index">
+        <ion-card>
+          <ion-item v-for="item of shipGroup.items" :key="item.id" lines="full">
+            <ion-thumbnail slot="start">
+              <Image :src="getProduct(item.productId).mainImageUrl" />
+            </ion-thumbnail>
+            <ion-label slot="start">
+              <p>{{item.brandName}}</p>
+              <h2>{{ item.name }}</h2>
+              <p>{{ $t("Color") }} : {{ $filters.getFeatures(getProduct(item.productId).featureHierarchy, '1/COLOR/') }}</p>
+              <p>{{ $t("Size") }} : {{ $filters.getFeatures(getProduct(item.productId).featureHierarchy, '1/SIZE/') }}</p>
+            </ion-label>
+            <ion-badge slot="end">{{item.status}}</ion-badge>
+          </ion-item>
+          <ion-item>
+            <ion-label>{{ shipGroup.shipmentMethodTypeId }} </ion-label>
+            <ion-button @click="changePickupPreference"  color="medium" fill="outline">{{ $t("Change") }}</ion-button>
+          </ion-item>
+          <ion-item>
+            <ion-list>
+            <ion-label>{{ shipGroup.shipTo.postalAddress.toName }}</ion-label>
+            <ion-label>{{ shipGroup.shipTo.postalAddress.address1 }} </ion-label>
+            <ion-label>{{ shipGroup.shipTo.postalAddress.city}} {{ shipGroup.shipTo.postalAddress.country}} {{ shipGroup.shipTo.postalAddress.postalCode}}</ion-label>
+            </ion-list>
+            <ion-button  slot="end" color="medium" fill="outline" @click="updateShipmentAddress">{{ $t("Edit") }}</ion-button>
+          </ion-item>
+          <ion-item lines="none">
+            <ion-label color="danger">{{ $t("Cancel Item") }}</ion-label>
+          </ion-item>
+        </ion-card>
+      </div>
     </ion-content>
   </ion-page>
 </template>
@@ -86,7 +86,7 @@ export default defineComponent({
   computed: {
     ...mapGetters({
       products: 'product/fetchProducts',
-      order: 'order/getCurrent',
+      order: 'order/getOrder',
       getProduct: 'product/getProduct'
     })
   },
