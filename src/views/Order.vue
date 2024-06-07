f1b1205a7a7dbc130c5b59f8a806fc40e74b399d
Yash Maheshwari <yashmaheshwari551@gmail.com>
2024-06-07 16:35:06 +0530

    Removed: unwanted console statement

--- src/views/Order.vue
+++ src/views/Order.vue
@@ -333,7 +333,6 @@ export default defineComponent({
         if (result.role) {
           // role will have the passed data
           shipGroup.updatedAddress = result.role
-          console.log('shipGroup.updatedAddress', shipGroup.updatedAddress)
         }
       });
       return modal.present();
